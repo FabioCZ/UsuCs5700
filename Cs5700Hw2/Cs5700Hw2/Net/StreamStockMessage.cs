f523abc223afa28f45f65fb62a0bef389b44484b
Fabio Gottlicher <g.fabio.cz@gmail.com>
2016-09-30 17:49:53 -0600

    progress on net stuff

---
+++ Cs5700Hw2/Cs5700Hw2/Net/StreamStockMessage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Cs5700Hw2.Model;
+
+namespace Cs5700Hw2.Net
+{
+    public class StreamStockMessage
+    {
+        public List<string> Tickers { get; private set; }
+
+        public int TickerCount => Tickers?.Count ?? 0;
+
+        public StreamStockMessage(List<Company> companies)
+        {
+            Tickers = companies?.Select(e => e.TickerName).ToList();
+        }
+
+        public byte[] ToBytes()
+        {
+            var bytes = new List<byte>();
+            var networkBytesCount = IPAddress.HostToNetworkOrder(Convert.ToInt16(TickerCount));
+            bytes.AddRange(BitConverter.GetBytes(networkBytesCount));
+            if (Tickers == null || Tickers.Count == 0)
+            {
+                return bytes.ToArray();
+            }
+
+            foreach (var t in Tickers)
+            {
+                bytes.AddRange(Encoding.ASCII.GetBytes(t.PadRight(6)));
+            }
+            return bytes.ToArray();
+        }
+
+        public byte[] EmptyMessage()
+        {
+            var bytes = new List<byte>();
+            var networkBytesCount = IPAddress.HostToNetworkOrder(Convert.ToInt16(0));
+            bytes.AddRange(BitConverter.GetBytes(networkBytesCount));
+            return bytes.ToArray();
+        }
+
+        //public static StreamStockMessage FromBytes()
+        //{
+            
+        //}
+
+
+    }
+
+    public static class TickerMessageHelper
+    {
+        public static decimal PenniesToDollars(this int pennies)
+        {
+            return new decimal(pennies)/100;
+        }
+    }
+}
---
+++ Cs5700Hw2/Cs5700Hw2/Net/TickerMessage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Net.IPAddress;
+
+namespace Cs5700Hw2.Net
+{
+    public class TickerMessage
+    {
+        public string TickerName { get; private set; }
+        public DateTime? Timestamp { get; private set; }
+        public int OpeningPrice { get; private set; }
+        public int PrevOpeningPrice { get; private set; }
+        public int PrevClosingPrice { get; private set; }
+        public int CurrPrice { get; private set; }
+        public int BidPrice { get; private set; }
+        public int AskPrice { get; private set; }
+        public int CurrVolume { get; private set; }
+        public int AvgTenDayVolume { get; private set; }
+
+        public TickerMessage(byte[] bytes)
+        {
+            Decode(bytes);
+        }
+
+        private void Decode(byte[] bytes)
+        {
+            if (bytes == null || bytes.Length != 42) return;
+
+            var reader = new BinaryReader(new MemoryStream(bytes));
+            TickerName = Encoding.ASCII.GetString(reader.ReadBytes(6));
+            Timestamp = new DateTime(NetworkToHostOrder(reader.ReadInt64()));
+            OpeningPrice = NetworkToHostOrder(reader.ReadInt32());
+            PrevOpeningPrice = NetworkToHostOrder(reader.ReadInt32());
+            PrevClosingPrice = NetworkToHostOrder(reader.ReadInt32());
+            CurrPrice = NetworkToHostOrder(reader.ReadInt32());
+            BidPrice = NetworkToHostOrder(reader.ReadInt32());
+            AskPrice = NetworkToHostOrder(reader.ReadInt32());
+            CurrVolume = NetworkToHostOrder(reader.ReadInt32());
+            AvgTenDayVolume = NetworkToHostOrder(reader.ReadInt32());
+        }
+
+
+
+    }
+}
