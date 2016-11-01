7ea62e5c4e339be48b17226f43295476124b750e
Fabio Gottlicher <g.fabio.cz@gmail.com>
2016-10-31 22:55:59 -0600

    scaling should be easier than this

--- Cs5700Hw3/Cs5700Hw3/Drawables/DrawableWithState.cs
+++ Cs5700Hw3/Cs5700Hw3/Drawables/DrawableWithState.cs
@@ -1,21 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace Cs5700Hw3.Drawables
 {
     public class DrawableWithState : IDrawable
     {
         private SimpleDrawable backingdrawable;
+        private float scale;
+        private Bitmap map;
+        private Size originalSize;
         public string FileName => backingdrawable.FileName;
         public string ReadableName => backingdrawable.ReadableName;
         public bool IsSelected { get; set; }
 
-        public Image Image { get; private set; }
         public Point Location { get; set; }
+        public Size Size { get; private set; }
+
+        public float Scale
+        {
+            get { return scale; }
+            set
+            {
+                if (scale < 0 || scale > 10)
+                {
+                    throw  new ArgumentException("bad scale");
+                }
+                scale = value;
+
+            }
+        }
 
         public DrawableWithState(IDrawable bd)
         {
@@ -24,11 +44,19 @@ namespace Cs5700Hw3.Drawables
                 throw new ArgumentException("Add command parameter should be a simple drawable");
             }
             backingdrawable = bd as SimpleDrawable;
-            Image = System.Drawing.Image.FromFile(FileName);
+            var img = System.Drawing.Image.FromFile(FileName);
+            map = new Bitmap(img);
+            Size = img.Size;
+            originalSize = img.Size;
         }
         public void Draw(Graphics graphics)
         {
-            graphics.DrawImage(Image, Location);
+            //var x = graphics.VisibleClipBounds.Width;
+            //var y = graphics.VisibleClipBounds.Height;
+            //graphics.ScaleTransform(scale, scale);
+            //graphics.TranslateTransform(-1*(x-graphics.VisibleClipBounds.Width),-1*(y - graphics.VisibleClipBounds.Height));
+            graphics.DrawImage(map, Location);
+            graphics.ResetTransform();
         }
     }
 }
