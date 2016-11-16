f40047cb6a85dad829fcf9e8e1ac05f0378e1d68
Fabio Gottlicher <g.fabio.cz@gmail.com>
2016-11-15 19:10:49 -0700

    started HW4

---
+++ Cs5700Hw4/Cs5700Hw4.Runner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs5700Hw4.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
